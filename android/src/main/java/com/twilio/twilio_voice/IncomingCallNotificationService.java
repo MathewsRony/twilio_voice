4c7fa551ca2bb319d499a5a73a5e9f37551d189a
Mathew <mrk14994@gmail.com>
2022-04-04 15:52:39 +0530

    Update IncomingCallNotificationService.java

--- android/src/main/java/com/twilio/twilio_voice/IncomingCallNotificationService.java
+++ android/src/main/java/com/twilio/twilio_voice/IncomingCallNotificationService.java
@@ -146,9 +146,10 @@ public class IncomingCallNotificationService extends Service {
         acceptIntent.putExtra(Constants.INCOMING_CALL_INVITE, callInvite);
         acceptIntent.putExtra(Constants.INCOMING_CALL_NOTIFICATION_ID, notificationId);
         PendingIntent piAcceptIntent = PendingIntent.getService(getApplicationContext(), 0, acceptIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         long[] mVibratePattern = new long[]{0, 400, 400, 400, 400, 400, 400, 400};
         Notification.Builder builder =
-                new Notification.Builder(getApplicationContext(), "high_importance_channel")
+                new Notification.Builder(getApplicationContext(), channelId)
                         .setSmallIcon(R.drawable.ic_call_end_white_24dp)
                         .setContentTitle(title)
                         .setContentText(text)
@@ -183,7 +184,6 @@ public class IncomingCallNotificationService extends Service {
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.createNotificationChannel(callInviteChannel);
 
-//        return channelId;
         return channelId;
     }
 
